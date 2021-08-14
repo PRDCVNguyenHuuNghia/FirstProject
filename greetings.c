2bbad431b6cdf8589d9ad1930dced2659c43218a
PRDCVNguyenHuuNghia <88831714+PRDCVNguyenHuuNghia@users.noreply.github.com>
2021-08-14 16:27:58 +0700

    update code

--- greetings.c
+++ greetings.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <string.h>
-#include "greetings.h"
-void get_greeting(char * buffer) {
+#include "NghiaNH_greetings.h"
+void NghiaNH_get_greeting(char * buffer) {
 
     if(buffer == NULL) {
         return;
